a2ef2ae006e2821389101c99e1a4cf7e77da29e8
Pardeep Poria <poriapardeep@gmail.com>
2019-02-21 20:36:38 +0530

    Wallet code moved to env quote fix

--- resources/views/layouts/sport.blade.php
+++ resources/views/layouts/sport.blade.php
@@ -79,7 +79,7 @@
 
 
                 var biaOptions = parseQuery(window.location.hash);
-                biaOptions.walletcode  = "{{ env(‘APP_SPORTSBOOK_WALLET_CODE’) }}";
+                biaOptions.walletcode  = "{{ env('APP_SPORTSBOOK_WALLET_CODE') }}";
                 biaOptions.token  = "{{ $player_token }}";
                 biaOptions.page = 'prelive';
                 biaOptions.getMarketsCallback = function (result) {
