9392b1d3005cf96967c20bb764956aad44c30021
vitalik <qb2v69xgd3>
2019-03-25 12:35:19 +0200

    fixes in sport layout

--- resources/views/layouts/sport.blade.php
+++ resources/views/layouts/sport.blade.php
@@ -150,43 +150,19 @@
 <div id="popup">
     <div class="container">
 
+        @include('partials.notification')
+
         @include('partials.provider-popup')
 
-        @if(!Auth::user())
+        @if(\Auth::user())
+            @include('partials.profile-popup')
+        @else
+            @include('partials.registration-popup')
             @include('partials.recover-password-popup')
             @include('partials.login-popup')
-        @else
-            @include('partials.profile-popup')
         @endif
 
-        <div class="simple-popup top-up-balance hidden">
-            <p class="h2" data-text="Пополнить баланс">Пополнить баланс</p>
-            <div class="max-w">
-                <form action="#" class="form">
-                    <div class="field">
-                        <select name="sel1" id="sel1" class="img-select">
-                            <option value="NETELLER" data-img="{{ asset('/img/uploads/select-img1.png') }}">NETELLER</option>
-                            <option value="NETELLER 2" data-img="{{ asset('/img/uploads/select-img2.png') }}">NETELLER 2</option>
-                            <option value="NETELLER 3" data-img="{{ asset('/img/uploads/select-img3.png') }}">NETELLER 3</option>
-                            <option value="NETELLER 4" data-img="{{ asset('/img/uploads/select-img4.png') }}">NETELLER 4</option>
-                        </select>
-                    </div>
-                    <div class="field">
-                        <input type="text" class="form-control" placeholder="Введите промокод">
-                        <p><a href="">Условия и правила</a></p>
-                    </div>
-                    <button type="button" class="btn full-width">выбрать</button>
-                </form>
-            </div>
-            <span class="js-close-popup" title="Закрыть"></span>
-        </div>
-
-        @include('partials.choose-game-popup')
-
-        @if(!Auth::user())
-            @include('partials.registration-popup')
-        @endif
-
+        @include('partials.deposit-popup')
         @include('partials.assistance-popup')
     </div>
 </div>
